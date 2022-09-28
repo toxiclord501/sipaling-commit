2979458739a640e5739bf47393f6f5eacae4fc77
Wahyu Amri <69815276+toxiclord501@users.noreply.github.com>
2022-09-29 00:24:51 +0700

    update my git info

--- task.sh
+++ task.sh
@@ -14,6 +14,6 @@ d=`date '+%Y-%m-%dT%H:%M:%SZ'`
 
 echo "## 🤔 LAST UPDATED AT: ${d}" > update.md
 
-git config --local user.email "i.xyzuan@gmail.com"
-git config --local user.name "xyzuan"
+git config --local user.email "wahyuamri501@gmail.com"
+git config --local user.name "toxiclord501"
 git commit -am "${arr[$rand]} (at ${d})"
